1803257d97814383c5ef351344d8968fc9e31bbb
Dan-Vy Le <danvyle85@gmail.com>
2019-03-24 17:12:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,11 @@
-def return
-  ask for input
-get input
-convert input to index
-if index is valid
-  make the move for index
-  show the board
-else
-  ask for input again until you get a valid input
-end
+def turn 
+
+#   ask for input
+# get input
+# convert input to index
+# if index is valid
+#   make the move for index
+#   show the board
+# else
+#   ask for input again until you get a valid input
+# end
