bd2e34d54ead72a57842e99c1995d539ee88edb3
Dan-Vy Le <danvyle85@gmail.com>
2019-03-24 17:17:27 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,8 @@
 def turn 
-
+  puts "Hello, welcome to Tic Tac Toe, it's player 1's turn, please enter a number from 1-9, 1 being upper left, 9 being bottom right."
+  input = gets.chomp
+  
+  
 #   ask for input
 # get input
 # convert input to index
@@ -9,3 +12,13 @@ def turn
 # else
 #   ask for input again until you get a valid input
 # end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
